ad737397ccde9bff844af829fd0ca63e068b46c0
Chao <chao1995@users.noreply.github.com>
2023-08-04 19:23:56 -0400

    setup.sh: install brew and other things

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 # remove all icons from the Dock
 defaults write com.apple.dock persistent-apps -array
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew install other things
+brew install coreutils  # for gls
+brew install --cask mactex
+brew install --cask google-chrome
+brew install --cask google-drive
